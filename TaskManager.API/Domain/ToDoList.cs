9bb5a696def0373115794d0291dd1b9c4be2eaa2
Randy Wiebe <randy_wiebe@hotmail.com>
2026-06-06 23:02:19 -0400

    Adds initial domain entities

---
+++ TaskManager.API/Domain/ToDoList.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManager.Domain
+{
+    public class ToDoList
+    {
+        public string Summary { get; private set; }
+
+        public ToDoTask[] Tasks { get; private set; }
+
+        public static ToDoList Create(string summary)
+        {
+            return new ToDoList { Summary = summary, Tasks = Array.Empty<ToDoTask>() };
+        }
+    }
+}
---
+++ TaskManager.API/Domain/ToDoTask.cs
@@ -0,0 +1,23 @@
+﻿namespace TaskManager.Domain
+{
+    /// <summary>
+    /// Individual tasks making up a to-do list.
+    /// Sadly the name `Task` was already taken...
+    /// </summary>
+    public class ToDoTask
+    {
+        public string Summary { get; private set; }
+        public bool Complete { get; private set; }
+        public DateTimeOffset? DueDate { get; private set; }
+
+        public static ToDoTask Create(string summary)
+        {
+            return new ToDoTask { Summary = summary };
+        }
+
+        public static ToDoTask Create(string summary, DateTimeOffset dueDate)
+        {
+            return new ToDoTask { Summary = summary, DueDate = dueDate };
+        }
+    }
+}
